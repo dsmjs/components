184102ce71fab77c80b294e9f4aabd41cdb6a4e7
Matt Travi <programmer@travi.org>
2017-12-10 20:42:31 -0600

    build(rollup): configured rollup to create esm and cjs bundles

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import babel from 'rollup-plugin-babel';
+import json from 'rollup-plugin-json';
+import nodeResolve from 'rollup-plugin-node-resolve';
+
+export default {
+  input: 'src/index.js',
+  sourcemap: true,
+  external: [
+    'react',
+    'react-helmet',
+    'prop-types',
+    'glamorous'
+  ],
+  plugins: [
+    nodeResolve({
+      module: true,
+      jsnext: true
+    }),
+    json(),
+    babel({
+      babelrc: false,
+      exclude: ['./node_modules/**'],
+      presets: [['env', {targets: {node: 'current', browsers: ['last 2 versions']}, modules: false}], 'react'],
+      plugins: [
+        ["transform-object-rest-spread", { "useBuiltIns": true }],
+        ['transform-react-remove-prop-types', {mode: 'wrap'}]
+      ]
+    })
+  ],
+  output: [
+    {file: 'lib/components.cjs.js', format: 'cjs'},
+    {file: 'lib/components.es.js', format: 'es'}
+  ]
+};
