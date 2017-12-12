4d86bd2cdbd74014b26f1ce3fff9a892e44950f6
Matt Travi <programmer@travi.org>
2017-12-11 22:12:09 -0600

    fix(global-styles): defined font and page dimensions
    also adjusted the header size to be closer to the legacy site

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,15 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
 
-import { configure } from '@storybook/react';
+import React from 'react';
+import {configure, addDecorator} from '@storybook/react';
+import {Div} from 'glamorous';
+import globalStyles from '../src/styles.json';
+
+addDecorator(story => (
+  <Div css={globalStyles}>
+    {story()}
+  </Div>
+));
 
 function loadStories() {
   const req = require.context('../src', true, /stories.js$/);
--- src/organisms/layout/index.js
+++ src/organisms/layout/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import {Div} from 'glamorous';
 import Header from '../../molecules/header';
 import Footer from '../footer';
+import globalStyles from '../../styles.json';
 
 export default function Layout({sponsor, location, children}) {
   return (
-    <div>
+    <Div css={{...globalStyles, marginTop: '1em', maxWidth: 1000, margin: '0 auto'}}>
       <Header sponsor={sponsor} location={location} />
       {children}
       <Footer />
-    </div>
+    </Div>
   );
 }
