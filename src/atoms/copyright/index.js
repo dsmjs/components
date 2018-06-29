e841a1c633318b762a8744352efb0fd58c28a14b
Matt Travi <programmer@travi.org>
2018-06-29 00:55:28 -0500

    fix(footer-styles): styled the links and copyright text styles

--- src/atoms/copyright/index.js
+++ src/atoms/copyright/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const styles = {backgroundColor: '#333', color: '#f0da4f', padding: '1em', textAlign: 'center'};
+const styles = {backgroundColor: '#333', color: '#f0da4f', padding: '1em', textAlign: 'center', fontWeight: 700};
 
 export default function CopyrightBar() {
   return <div style={styles}>Copyright © dsmJS 2013 - {new Date().getFullYear()}</div>;
--- src/atoms/navigation/site/index.js
+++ src/atoms/navigation/site/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import {shape} from 'prop-types';
+import {css} from 'glamor';
 
 const listItemStyles = {padding: '0.5em'};
 
 export default function SiteNavigation({linkStyles}) {
+  const enhancedLinkStyles = {
+    fontWeight: 700,
+    textDecoration: 'none',
+    ':hover': {textDecoration: 'underline'},
+    ...linkStyles
+  };
+
   return (
     <ol style={{listStyle: 'none', display: 'flex', margin: 0}}>
-      <li style={listItemStyles}><a style={linkStyles} href="/">Home</a></li>
-      <li style={listItemStyles}><a style={linkStyles} href="/archive">Past Meetings</a></li>
+      <li className={css(listItemStyles)}><a className={css(enhancedLinkStyles)} href="/">Home</a></li>
+      <li className={css(listItemStyles)}><a className={css(enhancedLinkStyles)} href="/archive">Past Meetings</a></li>
     </ol>
   );
 }
