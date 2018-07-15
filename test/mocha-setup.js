b6bddf0d44cfe529762783ba72b7bb3a0be124ed
Matt Travi <programmer@travi.org>
2018-07-15 02:08:02 -0500

    test(console): added thrown errors when console error/warn are called
    so that react warnings are dealt with rather than becoming noise

--- test/mocha-setup.js
+++ test/mocha-setup.js
@@ -3,3 +3,6 @@ import {configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({adapter: new Adapter()});
+
+console.error = err => { throw new Error(err); };           // eslint-disable-line no-console
+console.warn = warning => { throw new Error(warning); };    // eslint-disable-line no-console
