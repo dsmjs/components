89e3f000d7ae338cf110347b6e63f4946d2b7fb0
Matt Travi <programmer@travi.org>
2018-08-23 23:33:35 -0500

    docs(social): defined scenarios that will show social handles

--- src/molecules/talk/stories.js
+++ src/molecules/talk/stories.js
@@ -5,14 +5,29 @@ import any from '@travi/any';
 import {talkContent} from '../../../.storybook/data/html-content';
 import Talk from './index';
 
-storiesOf('Molecules/Talk', module).add('default', () => (
-  <Talk
-    talk={{
-      title: any.sentence(),
-      speaker: {frontmatter: {name: `${any.word()} ${any.word()}`}},
-      twitter: any.word(),
-      slides: any.url()
-    }}
-    content={talkContent}
-  />
-));
+const talk = {
+  title: any.sentence(),
+  speaker: {frontmatter: {name: `${any.word()} ${any.word()}`, twitter: any.word(), github: any.word()}},
+  slides: any.url()
+};
+const talkNoGithub = {
+  title: any.sentence(),
+  speaker: {frontmatter: {name: `${any.word()} ${any.word()}`, twitter: any.word()}},
+  slides: any.url()
+};
+const talkNoTwitter = {
+  title: any.sentence(),
+  speaker: {frontmatter: {name: `${any.word()} ${any.word()}`, github: any.word()}},
+  slides: any.url()
+};
+const talkNoSocial = {
+  title: any.sentence(),
+  speaker: {frontmatter: {name: `${any.word()} ${any.word()}`}},
+  slides: any.url()
+};
+
+storiesOf('Molecules/Talk', module)
+  .add('default', () => <Talk talk={talk} content={talkContent} />)
+  .add('no twitter', () => <Talk talk={talkNoTwitter} content={talkContent} />)
+  .add('no github', () => <Talk talk={talkNoGithub} content={talkContent} />)
+  .add('no social', () => <Talk talk={talkNoSocial} content={talkContent} />);
