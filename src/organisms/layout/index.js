d9496f591c7faff0f775cb3863800f33e000efa8
Matt Travi <programmer@travi.org>
2018-08-23 23:29:00 -0500

    style(inner-gutter): applied the gutter to the content

--- src/organisms/layout/index.js
+++ src/organisms/layout/index.js
@@ -4,12 +4,15 @@ import {Div} from 'glamorous';
 import Header from '../../molecules/header';
 import Footer from '../footer';
 import globalStyles from '../../styles.json';
+import layoutStyles from '../../layoutStyles.json';
 
 export default function Layout({sponsor, location, children}) {
   return (
     <Div css={{...globalStyles, maxWidth: 1000, margin: '1em auto', backgroundColor: '#fff'}}>
       <Header sponsor={sponsor} location={location} />
-      {children}
+      <div style={{padding: `0 ${layoutStyles.innerGutterWidth}px`}}>
+        {children}
+      </div>
       <Footer />
     </Div>
   );
