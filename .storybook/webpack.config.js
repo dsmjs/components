b94e0ccbe107859ec52e656d9575716e8d7d7420
Matt Travi <developer@travi.org>
2019-06-01 21:28:38 -0500

    docs(storybook): switched the babel config to use the dsmjs org config
    and got the rest of storybook back to working and upgraded

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack');
 
-module.exports = (baseConfig, env, defaultConfig) => {
-  defaultConfig.plugins.push(new webpack.DefinePlugin({'process.env': {ANY_SEED: JSON.stringify(0.4070123094134033)}}));
+module.exports = ({config}) => {
+  config.plugins.push(new webpack.DefinePlugin({'process.env': {ANY_SEED: JSON.stringify(0.4070123094134033)}}));
 
-  return defaultConfig;
+  return config;
 };
