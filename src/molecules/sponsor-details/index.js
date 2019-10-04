0223edaffd29082a4f91716a44f07ae36e10256f
Matt Travi <developer@travi.org>
2019-10-04 14:32:37 -0500

    test(eslint): accounted for the whitespace rule adjustment

--- src/molecules/sponsor-details/index.js
+++ src/molecules/sponsor-details/index.js
@@ -4,7 +4,8 @@ import {shape, string} from 'prop-types';
 export default function SponsorDetails({sponsor}) {
   return (
     <p>
-      {'Pizza and drinks proudly sponsored by '}
+      Pizza and drinks proudly sponsored by
+      {' '}
       <a href={sponsor.site}>{sponsor.name}</a>
     </p>
   );
