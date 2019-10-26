c11bd402de611db8f8e8e759eee1178ac4c71391
mbeche <mb.johnsonbece@gmail.com>
2019-10-25 22:38:44 -0500

    style(social-icons): increases icon size. Added space. Vertically center
    I didn't want to change the styling on `ExternalLink` so I added an additional div and added some
    styles.

--- src/atoms/social/speaker/presentational.js
+++ src/atoms/social/speaker/presentational.js
@@ -4,7 +4,7 @@ import Icon from 'react-simple-icons';
 import {useHover} from 'react-use';
 import ExternalLink from '../../links/external';
 
-const iconSize = 16;
+const iconSize = 24;
 const listStyles = {
   listStyle: 'none',
   display: 'flex',
@@ -13,12 +13,18 @@ const listStyles = {
   padding: 0,
   fontSize: 20
 };
+
 const listItemStyles = {
-  padding: '0 0 0 .5em',
+  padding: '0 0 0 1em',
   height: iconSize,
   span: {
     paddingLeft: '.25em'
   }
+};
+
+const socialItemStyle = {
+  display: 'flex',
+  alignItems: 'center'
 };
 
 export default function SpeakerSocialIcons({twitter, github}) {
@@ -26,16 +32,20 @@ export default function SpeakerSocialIcons({twitter, github}) {
   const TwitterIcon = hovered => (
     <li css={listItemStyles}>
       <ExternalLink to={`https://twitter.com/${twitter}`}>
-        <Icon fill={hovered ? '#1DA1F2' : nonHoveredIconColor} size={iconSize} name="twitter" />
-        <span>{twitter}</span>
+        <div css={socialItemStyle}>
+          <Icon fill={hovered ? '#1DA1F2' : nonHoveredIconColor} size={iconSize} name="twitter" />
+          <span>{twitter}</span>
+        </div>
       </ExternalLink>
     </li>
   );
   const GithubIcon = hovered => (
     <li css={listItemStyles}>
       <ExternalLink to={`https://github.com/${github}`}>
-        <Icon fill={hovered ? '#000' : nonHoveredIconColor} size={iconSize} name="github" />
-        <span>{github}</span>
+        <div css={socialItemStyle}>
+          <Icon fill={hovered ? '#000' : nonHoveredIconColor} size={iconSize} name="github" />
+          <span>{github}</span>
+        </div>
       </ExternalLink>
     </li>
   );
