f95d0fb76a1d271300af728799664a6c46ff9f3a
Matt Travi <programmer@travi.org>
2020-06-16 16:38:40 -0500

    feat(archive-pagination): highlighted the active page

--- src/molecules/archive/pagination/component.js
+++ src/molecules/archive/pagination/component.js
@@ -11,14 +11,22 @@ const listStyles = {
   li: {display: 'inline-block'}
 };
 
-export default function Pagination({totalPages}) {
+function isActivePage(currentPage, pageNumber) {
+  return currentPage === pageNumber;
+}
+
+function highlightWhenActive(currentPage, pageNumber) {
+  return isActivePage(currentPage, pageNumber) ? '#ced' : undefined;
+}
+
+export default function Pagination({totalPages, currentPage}) {
   return (
     <ol css={listStyles}>
       {
         Array
           .from({length: totalPages}, (_, index) => ({pageNumber: index + 1}))
           .map(({pageNumber}) => (
-            <li key={`page-${pageNumber}`}>
+            <li key={`page-${pageNumber}`} css={{backgroundColor: highlightWhenActive(currentPage, pageNumber)}}>
               <InternalLink to={1 === pageNumber ? '/archive' : `/archive/page-${pageNumber}`}>
                 {`${pageNumber}`}
               </InternalLink>
@@ -30,5 +38,6 @@ export default function Pagination({totalPages}) {
 }
 
 Pagination.propTypes = {
-  totalPages: number.isRequired
+  totalPages: number.isRequired,
+  currentPage: number.isRequired
 };
--- src/organisms/archive/stories.js
+++ src/organisms/archive/stories.js
@@ -40,7 +40,11 @@ export default {
   ]
 };
 
-export const Default = () => <Archive meetings={meetings} totalPages={any.integer({max: 20})} />;
+export const Default = () => {
+  const totalPages = any.integer({max: 20});
+
+  return <Archive meetings={meetings} totalPages={totalPages} currentPage={any.integer({max: totalPages})} />;
+};
 
 Default.story = {
   name: 'default'
